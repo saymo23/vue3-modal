333ba8f2d32076b5db06c8507e7c83bb1fc942c0
Daniel_S <daniel@mexiconow.mx>
2023-09-21 16:27:10 -0600

    Primer Comit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import { ref } from 'vue'
+import Modal from './components/Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  setup(){
+    const isOpen = ref(false)
+
+    const openModal = () => {
+      isOpen.value = true
+      //HERE OTHER CHANGES
+
+      return isOpen.value
+    }
+
+    return{
+      isOpen, openModal
+    }
+  }
+}
+
+</script>
+
+<template>
+  <button @click="openModal">
+    Open Modal
+  </button>
+  <Modal :open="isOpen" @close="isOpen = !isOpen" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="background" v-if="open">
+    <div class="card">
+      Hello World!!!
+      <button @click="$emit('close')">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      type: Boolean,
+      required: true
+    },
+    setup(props, { emit }){
+      const closeModal = () => {
+        emit('close', false)
+      }
+      return {
+        closeModal
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.background{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(20,20,20,0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card{
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  color: black;
+  width: 500px;
+  height: 300px;
+  padding: 1rem;
+  border-radius: 2rem;
+  justify-content: space-between;
+}
+</style>
